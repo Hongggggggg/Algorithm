61e2eae2d4a35006a0bffe7a6824ea4346f8cfec
Hongggggggg <hongggggggghammer@gmail.com>
2021-08-18 20:46:01 +0800

    bubble sort

---
+++ 排序/bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(nums):
+    for i in range(len(nums) - 1):
+        for j in range(len(nums) - 1 - i):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+
+print(bubble_sort([5, 6, 9, 2, 4, 3, 7]))
