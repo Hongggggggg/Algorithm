ca86d5fa01298e96cb9d02d89cdccce8a2f1bb12
Hongggggggg <hongggggggghammer@gmail.com>
2021-10-20 10:04:51 +0800

    -invertTree

---
+++ 二叉树/invertTree.py
@@ -0,0 +1,44 @@
+#https://leetcode-cn.com/problems/invert-binary-tree/
+#递归法前序遍历
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+#迭代法前序遍历
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return root
+        st = []
+        st.append(root)
+        while st:
+            node = st.pop()
+            node.left, node.right = node.right, node.left
+            if node.right:
+                st.append(node.right)
+            if node.left:
+                st.append(node.left)
+        return root
+
+#迭代法广度优先遍历
+class Solution:
+    def invertTree(self, root):
+        import collections
+        queue = collections.deque()
+        if root:
+            queue.append(root)
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                node.left, node.right = node.right, node.left
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
