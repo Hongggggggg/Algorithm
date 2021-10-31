85c8d33498e08e237d8b64978a1e98b83eefc9e6
Hongggggggg <hongggggggghammer@gmail.com>
2021-10-31 10:21:05 +0800

    -sumOfLeftLeaves

---
+++ 二叉树/sumOfLeftLeaves.py
@@ -0,0 +1,42 @@
+#https://leetcode-cn.com/problems/sum-of-left-leaves/
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+#递归
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        left_left_leaves_sum = self.sumOfLeftLeaves(root.left) #左
+        right_right_leaves_sum = self.sumOfLeftLeaves(root.right) #右
+
+        cur_left_leaf_val = 0
+        if root.left and not root.left.left and not root.left.right:
+            cur_left_leaf_val = root.left.val
+
+        return cur_left_leaf_val + left_left_leaves_sum + right_right_leaves_sum #中
+
+#迭代法
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        stack = []
+        if root:
+            stack.append(root)
+        res = 0
+
+        while stack:
+            #每次都把当前节点的左节点加进去
+            cur_node = stack.pop()
+            if cur_node.left and not cur_node.left.left and not cur_node.left.right:
+                res += cur_node.left.val
+
+            if cur_node.left:
+                stack.append(cur_node.left)
+            if cur_node.right:
+                stack.append(cur_node.right)
+        return res
