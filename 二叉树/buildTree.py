cbc891a2682d1a0f4333bd249cea18d0639a8d39
Hongggggggg <hongggggggghammer@gmail.com>
2021-11-02 10:01:43 +0800

    -Build tree via preorder and inorder

--- 二叉树/buildTree.py
+++ 二叉树/buildTree.py
@@ -34,3 +34,32 @@ class Solution:
         root.right = self.buildTree(inorder_right, postorder_right)
 
         return root
+
+
+class Solution:
+    def buildTree(self, preorder, inorder) -> TreeNode:
+        # 第一步: 特殊情况讨论: 树为空. 或者说是递归终止条件
+        if not preorder: 
+            return None
+        
+        # 第二步: 前序遍历的第一个就是当前的中间节点. 
+        root_val = preorder[0]
+        root = TreeNode(root_val)
+
+        # 第三步: 找切割点. 
+        separator_idx = inorder.index(root_val)
+
+        # 第四步: 切割inorder数组. 得到inorder数组的左,右半边. 
+        inorder_left = inorder[:separator_idx]
+        inorder_right = inorder[separator_idx + 1:]
+
+        # 第五步: 切割preorder数组. 得到preorder数组的左,右半边.
+        # ⭐️ 重点1: 中序数组大小一定跟前序数组大小是相同的. 
+        preorder_left = preorder[1:1 + len(inorder_left)]
+        preorder_right = preorder[1 + len(inorder_left):]
+
+        # 第六步: 递归
+        root.left = self.buildTree(preorder_left, inorder_left)
+        root.right = self.buildTree(preorder_right, inorder_right)
+
+        return root
