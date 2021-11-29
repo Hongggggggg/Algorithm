7a5ce05dd05b995ebd9f03dbc3f45128abb8484b
Hongggggggg <hongggggggghammer@gmail.com>
2021-11-29 20:14:47 +0800

    -findtinerary

---
+++ 回溯/findItinerary.py
@@ -0,0 +1,34 @@
+#https://leetcode-cn.com/problems/reconstruct-itinerary/
+from collections import defaultdict
+
+class Solution:
+    def findItinerary(self, tickets):
+        # defaultdic(list) 是为了方便直接append
+        tickets_dict = defaultdict(list)
+        for item in tickets:
+            tickets_dict[item[0]].append(item[1])
+
+        '''
+        tickets_dict里面的内容是这样的
+         {'JFK': ['SFO', 'ATL'], 'SFO': ['ATL'], 'ATL': ['JFK', 'SFO']})
+        '''
+
+        path = ["JFK"]
+        def backtracking(start_point):
+            #终止条件
+            if len(path) == len(tickets) + 1:
+                return True
+            tickets_dict[start_point].sort()
+            for _ in tickets_dict[start_point]:
+                #必须及时删除，避免出现死循环
+                end_point = tickets_dict[start_point].pop(0)
+                path.append(end_point)
+                #只要找到一个就可以返回
+                if backtracking(end_point):
+                    return True
+                path.pop()
+                tickets_dict[start_point].append(end_point)
+        backtracking("JFK")
+        return path
+
+
