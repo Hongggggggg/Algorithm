17cae145851e828572ecc74b991f9894a6d210b1
Hongggggggg <hongggggggghammer@gmail.com>
2021-12-08 20:08:33 +0800

    -canCompleteCircuit

---
+++ 贪心/canCompleteCircuit.py
@@ -0,0 +1,18 @@
+#https://leetcode-cn.com/problems/gas-station/
+
+
+from typing import List
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start = 0
+        curSum = 0
+        totalSum = 0
+        for i in range(len(gas)):
+            curSum += gas[i] - cost[i]
+            totalSum += gas[i] - cost[i]
+            if curSum < 0:
+                curSum = 0
+                start = i + 1
+        if totalSum < 0: return -1
+        return start
