e52dd5b1d084efee6735c021069e422897053e32
Hongggggggg <hongggggggghammer@gmail.com>
2021-12-13 19:42:35 +0800

    -monotoneIncreasingDigits

---
+++ 贪心/monotoneIncreasingDigits.py
@@ -0,0 +1,9 @@
+#https://leetcode-cn.com/problems/monotone-increasing-digits/
+class Solution:
+    def monotoneIncreasingDigits(self, n: int) -> int:
+        a = list(str(n))
+        for i in range(len(a) - 1, 0, -1):
+            if int(a[i]) < int(a[i - 1]):
+                a[i - 1] = str(int(a[i - 1]) - 1)
+                a[i:] = '9' * (len(a) - i)
+        return int("".join(a))
