c4bfa076e6302f504fae44c2314c9bdac11b2f90
Hongggggggg <hongggggggghammer@gmail.com>
2021-12-14 19:50:33 +0800

    -minCostClimbingStairs

---
+++ 动态规划/minCostClimbingStairs.py
@@ -0,0 +1,12 @@
+#https://leetcode-cn.com/problems/min-cost-climbing-stairs/
+from typing import List
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+        return min(dp[len(cost) - 1], dp[len(cost) - 2])
