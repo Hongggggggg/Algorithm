75580dcdee0bb38278d4ba5c19670fa6e468bd87
Hongggggggg <hongggggggghammer@gmail.com>
2021-12-19 12:12:18 +0800

    -canPartition

---
+++ 动态规划/canPartition.py
@@ -0,0 +1,14 @@
+#https://leetcode-cn.com/problems/last-stone-weight-ii/
+from typing import List
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        target = sum(nums)
+        if target % 2 == 1: return False
+        target //= 2
+        dp = [0] * (target + 1)
+        for i in range(len(nums)):
+            for j in range(target, nums[i] - 1, -1):
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i])
+        return target == dp[target]
+
