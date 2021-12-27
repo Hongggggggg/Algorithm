cfabd59fc552422bb211d216e04076032701cac4
Hongggggggg <hongggggggghammer@gmail.com>
2021-12-27 21:02:49 +0800

    -coinChange

---
+++ 动态规划/coinchange.py
@@ -0,0 +1,13 @@
+#https://leetcode-cn.com/problems/coin-change/
+
+from typing import List
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            for j in range(coin, amount + 1):
+                dp[j] = min(dp[j], dp[j - coin] + 1)
+        return dp[amount] if dp[amount] < amount + 1 else -1
