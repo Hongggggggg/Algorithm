91c29938f7db113624cdbec22b17d4553941519a
Hongggggggg <hongggggggghammer@gmail.com>
2022-01-07 11:13:28 +0800

    -lengthOfLIS

---
+++ 动态规划/lengthOfLIS.py
@@ -0,0 +1,15 @@
+#https://leetcode-cn.com/problems/longest-increasing-subsequence/
+from typing import List
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
+        dp = [1] * len(nums)
+        result = 0
+        for i in range(1, len(nums)):
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+            result = max(result, dp[i])
+        return result
