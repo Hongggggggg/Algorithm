29d2ca6ad0a0c2a868dd42cff80712af8a9c6f0c
Hongggggggg <hongggggggghammer@gmail.com>
2022-02-12 12:00:07 +0800

    -hasCycle

---
+++ 链表/hasCycle.py
@@ -0,0 +1,20 @@
+from operator import truediv
+from xml.dom.pulldom import ErrorHandler
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow, fast = head, head
+        while(fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+
+            if(slow == fast):
+                return True
+        return False
+        
